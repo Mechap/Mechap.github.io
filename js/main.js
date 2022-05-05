f72080702107f524a097b4e0c8e808986ea8f3ad
mechap <simongregoire007@gmail.com>
2022-05-05 19:25:38 +0200

    remove jquery dependency

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-function draw() {
-    var canvas = document.getElementById('canvas_example');
-    var ctx = canvas.getContext('2d');
+window.addEventListener("scroll", (event) => {
+    let distanceScrolled = this.scrollY;
 
-    ctx.fillRect(30, 30, 100, 100);
-
-    for (let i = 0; i < 3; ++i) {
-        for (let j = 0; j < 3; ++j) {
-            ctx.save();
-
-            ctx.translate(10 + j * 50, 10 + i * 50);
-            ctx.fillRect(0, 0, 100, 100);
-            ctx.restore();
-        }
+    if (distanceScrolled >= 300) {
+        document.querySelector('nav').classList.add('fixed-header');
+        document.querySelector('nav div').classList.add('visible-title');
+    } else {
+        document.querySelector('nav').classList.remove('fixed-header');
+        document.querySelector('nav div').classList.remove('visible-title');
     }
-}
+
+    if (distanceScrolled >= 50) {
+        document.querySelector('.header-banner').classList.add('scaled-img');
+    } else {
+        document.querySelector('.header-banner').classList.remove('scaled-img');
+    }
+
+    if (distanceScrolled >= 80) {
+        document.querySelector('.header-banner').style.filter = 'blur(3px)';
+        console.log("fuck");
+    } else {
+        console.log("ok");
+        document.querySelector('.header-banner').style.filter = 'none';
+    }
+});
